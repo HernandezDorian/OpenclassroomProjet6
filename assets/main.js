80822c3542bd334f025765a8b07a2139e2389c67
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-10-11 13:57:20 +0200

    Ajout des filters via l'API

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,66 @@
-import { getWorks } from "./requests.js";
+import { getWorks, getCategories } from "./requests.js";
 
 let works = await getWorks();
 
+let categories = await getCategories();
+
 console.log(works);
+console.log(categories);
+
+function setFilter(){
+    for (let index = 0; index < categories.length; index++) {
+        console.log(categories[index].name)
+        const filtersec = document.querySelector('.filter-sec');
+        
+        const filterElement = document.createElement('button');
+        filterElement.classList.add('filter');
+        filterElement.id = "filtre_"+categories[index].id;
+        try {
+            filterElement.textContent = categories[index].name;
+            filtersec.appendChild(filterElement);
+        } catch (error) {
+            console.log("Erreur dans la nomination des éléments filtres dans l'API");
+        }
+    }
+}
+
+
+
+function filterSelecRm(id){
+    id.classList.remove("filter-selec");
+};
 
 function setProjets(){
 
     for (let index = 0; index < works.length; index++) {
-        console.log(works[index].id);
         const gallery = document.querySelector('.gallery');
         const figureElement = document.createElement('figure');
 
         const imageElement = document.createElement('img');
-        imageElement.src = works[index].imageUrl;
+        try {
+            imageElement.src = works[index].imageUrl;
+        } catch (error) {
+            console.log("Erreur dans l'url des images de projets dans l'API");
+        }
+        
 
         const figCaptionElement = document.createElement('figcaption');
-        figCaptionElement.textContent = works[index].title;
+        
+        try {
+            figCaptionElement.textContent = works[index].title;
+        } catch (error) {
+            console.log("Erreur dans la nomination des titres de projets dans l'API");
+        }
 
         gallery.appendChild(figureElement);
         figureElement.appendChild(imageElement);
         figureElement.appendChild(figCaptionElement);
+
         // gallery.innerHTML += `<figure><img src="${works[index].imageUrl}"><figcaption>${works[index].title}</figcaption></figure>`
         
     }
 
 };
 
+setFilter();
 setProjets();
--- assets/requests.js
+++ assets/requests.js
@@ -1,5 +1,11 @@
 export async function getWorks(){
     const repWorks = await fetch('http://localhost:5678/api/works')
-    let works = await repWorks.json();
-    return works;
+    let reponse = await repWorks.json();
+    return reponse;
+}
+
+export async function getCategories(){
+    const repCategories = await fetch('http://localhost:5678/api/categories')
+    let reponse = await repCategories.json();
+    return reponse;
 }
