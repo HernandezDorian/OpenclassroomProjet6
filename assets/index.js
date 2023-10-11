96bdc2bd7c91d6c02c3fa67e3bf23c077d2607d6
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-10-11 15:12:09 +0200

    Fonctionnement des filtres

---
+++ assets/index.js
@@ -0,0 +1,99 @@
+import { getWorks, getCategories } from "./requests.js";
+
+let works = await getWorks();
+
+let categories = await getCategories();
+
+
+console.log(works);
+console.log(categories);
+
+function setFilter(){
+    for (let index = 0; index < categories.length; index++) {
+        const filtersec = document.querySelector('.filter-sec');
+        
+        const filterElement = document.createElement('button');
+        filterElement.classList.add('filter');
+        filterElement.id = "filtre_"+categories[index].id;
+        try {
+            filterElement.textContent = categories[index].name;
+            filtersec.appendChild(filterElement);
+        } catch (error) {
+            console.log("Erreur dans la nomination des éléments filtres dans l'API");
+        }
+    }
+}
+
+function filterSelec(){
+    let actualselect = "filtre_0"
+    let filter_0 = works
+    let filter_1 = works.filter(item => item.category.id === 1);
+    let filter_2 = works.filter(item => item.category.id === 2);
+    let filter_3 = works.filter(item => item.category.id === 3);
+    for (let index = 0; index < categories.length + 1; index++) {
+        document.getElementById(`filtre_${index}`).addEventListener('click', function(){
+            document.getElementById(`filtre_${index}`).classList.add("filter-selec");
+            document.getElementById(actualselect).classList.remove("filter-selec");
+            actualselect = `filtre_${index}`
+            document.querySelector(".gallery").innerHTML = "";
+            
+            switch (actualselect) {
+                case "filtre_0":
+                    setProjets(filter_0);
+                    break;
+            
+                case "filtre_1":
+                    setProjets(filter_1);
+                    break;
+                    
+                case "filtre_2":
+                    setProjets(filter_2);
+                    break;
+                    
+                case "filtre_3":
+                    setProjets(filter_3);
+                    break;
+            }
+            
+        });
+    }
+
+};
+
+function filterSelecRm(id){
+    id.classList.remove("filter-selec");
+};
+
+function setProjets(listeProjets){
+    // switch (key) {
+    //     case 0:
+            for (let index = 0; index < listeProjets.length; index++) {
+                const gallery = document.querySelector('.gallery');
+                const figureElement = document.createElement('figure');
+                const imageElement = document.createElement('img');
+                try {
+                    imageElement.src = listeProjets[index].imageUrl;
+                } catch (error) {
+                    console.log("Erreur dans l'url des images de projets dans l'API");
+                }
+                const figCaptionElement = document.createElement('figcaption');
+                try {
+                    figCaptionElement.textContent = listeProjets[index].title;
+                } catch (error) {
+                    console.log("Erreur dans la nomination des titres de projets dans l'API");
+                }
+                gallery.appendChild(figureElement);
+                figureElement.appendChild(imageElement);
+                figureElement.appendChild(figCaptionElement);
+            }
+            // gallery.innerHTML += `<figure><img src="${works[index].imageUrl}"><figcaption>${works[index].title}</figcaption></figure>`
+    //     break;
+    
+        
+    // }
+
+};
+
+setFilter();
+setProjets(works);
+filterSelec();
