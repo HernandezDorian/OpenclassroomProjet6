89a4caf06bd1dee2e3b30d72e72ac97c10dd71d2
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-10-11 12:12:02 +0200

    Génération des projets via l'API
    en commentaire une autre méthode de génération ne sachant pas encore de laquelle je vais avoir besoin le plus pour la suite

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,29 @@
 import { getWorks } from "./requests.js";
 
-await getWorks();
+let works = await getWorks();
 
+console.log(works);
+
+function setProjets(){
+
+    for (let index = 0; index < works.length; index++) {
+        console.log(works[index].id);
+        const gallery = document.querySelector('.gallery');
+        const figureElement = document.createElement('figure');
+
+        const imageElement = document.createElement('img');
+        imageElement.src = works[index].imageUrl;
+
+        const figCaptionElement = document.createElement('figcaption');
+        figCaptionElement.textContent = works[index].title;
+
+        gallery.appendChild(figureElement);
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figCaptionElement);
+        // gallery.innerHTML += `<figure><img src="${works[index].imageUrl}"><figcaption>${works[index].title}</figcaption></figure>`
+        
+    }
+
+};
+
+setProjets();
--- assets/requests.js
+++ assets/requests.js
@@ -1,5 +1,5 @@
 export async function getWorks(){
     const repWorks = await fetch('http://localhost:5678/api/works')
     let works = await repWorks.json();
-    console.log(works);
+    return works;
 }
