fb59118801c774612c733c0ab93dd48230eb4abb
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-10-12 11:26:18 +0200

    Vérification de forme sur les éléments du formulaire login
    email = email valide
    mot de passe n'est pas vide

---
+++ assets/login.js
@@ -0,0 +1,58 @@
+let isAlreadyHeremail = false;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const submit = document.querySelector('#submit');
+
+function validateEmail(str) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const result = regex.test(str); 
+    return result;
+  }
+
+function incorrect(parent, message){
+        const div = document.querySelector(parent);
+        const error = document.createElement('p');
+        error.classList.add('errormessage');
+        error.textContent = message;
+        div.appendChild(error);
+        return isAlreadyHeremail = true; 
+};
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const errormessage = document.querySelectorAll(".errormessage")
+    console.log(errormessage)
+    if(errormessage.length > 0){
+        for (let index = 0; index < errormessage.length; index++) {
+            errormessage[index].parentNode.removeChild(errormessage[index]);
+        }
+    } 
+    
+    // Vérifier la validité du champ email
+    if (validateEmail(email.value)){
+        console.log(email.value + " " + password.value);
+    }
+    else {
+
+        email.classList.add('errorform');
+        incorrect("#maildiv","L'email n'est pas conforme")
+        sleep(500).then(() => { email.classList.remove('errorform'); });
+        console.log("erreur dans l'email");
+    }
+
+    if (password.value != ""){
+        console.log(email.value + " " + password.value);
+    }
+    else {
+
+        email.classList.add('errorform');
+        incorrect("#passdiv","Le mot de passe est vide")
+        sleep(500).then(() => { email.classList.remove('errorform'); });
+        console.log("erreur dans l'email");
+    }
+    
+});
